3f340e194db21af200ef080091c023894cf66cb6
Jonas Franke <jonasfranke@sdevs.org>
2024-07-23 10:01:32 +0200

    🎇 Windows compatible

--- utils/loadEvents.js
+++ utils/loadEvents.js
@@ -1,8 +1,10 @@
+import fs from "node:fs";
+import path from "node:path";
 import { basename } from "node:path";
 import { performance } from "node:perf_hooks";
-import { globby } from "globby";
 import { defaultConfig } from "../config.js";
 import { logger } from "./logger.js";
+import { pathToFileURL } from "node:url"; // Import pathToFileURL
 
 /**
  * Loads all events from the /events folder
@@ -15,16 +17,20 @@ export default async function loadEvents(client) {
  try {
   const loadTime = performance.now();
 
-  const events = await globby(`${process.cwd()}/events/*/*.js`);
-  for (const file of events) {
-   await import(file).then((e) => {
-    const eventName = basename(file, ".js");
-    defaultConfig.debugger.displayEventList && logger("event", `Loaded event ${eventName} from ${file.replace(process.cwd(), "")}`);
-    client.on(eventName, e[eventName].bind(null, client));
-   });
-  }
+  const eventDirectories = ["guild", "client"];
 
-  logger("event", `Loaded ${events.length} events from /events in ${Math.round(performance.now() - loadTime)}ms`);
+  for (const dir of eventDirectories) {
+   const events = fs.readdirSync(path.join(process.cwd(), "events", dir)).filter((file) => file.endsWith(".js"));
+   for (const file of events) {
+    const eventName = path.basename(file, ".js");
+    const filePath = path.join(process.cwd(), "events", dir, file);
+    const fileURL = pathToFileURL(filePath).href;
+    const event = await import(fileURL);
+    defaultConfig.debugger.displayEventList && logger("event", `Loaded event ${eventName} from /events/${dir}/${file}`);
+    client.on(eventName, event[eventName].bind(null, client));
+   }
+   logger("event", `Loaded ${events.length} events from /events/${dir} in ${Math.round(performance.now() - loadTime)}ms`);
+  }
  } catch (error) {
   logger("error", `Error loading events: ${error.message}`);
  }
