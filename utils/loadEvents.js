2929a844725aa5082a6c1851f707f09266790a97
Jonas Franke <jonasfranke@sdevs.org>
2024-07-23 10:06:40 +0200

    chore: Refactor loadEvents function to improve performance and readability

--- utils/loadEvents.js
+++ utils/loadEvents.js
@@ -1,35 +1,31 @@
 import fs from "node:fs";
 import path from "node:path";
-import { basename } from "node:path";
+import { pathToFileURL } from "node:url";
 import { performance } from "node:perf_hooks";
-import { defaultConfig } from "../config.js";
 import { logger } from "./logger.js";
-import { pathToFileURL } from "node:url"; // Import pathToFileURL
 
-/**
- * Loads all events from the /events folder
- *
- * @param {object} client - The Discord client
- * @returns {Promise<void>} Promise that resolves when all events are loaded
- * @throws {Error} Error that is thrown if an event could not be loaded
- */
 export default async function loadEvents(client) {
  try {
   const loadTime = performance.now();
+  const eventsPath = path.join(process.cwd(), "events");
+  const eventDirectories = fs.readdirSync(eventsPath).filter((dir) => fs.statSync(path.join(eventsPath, dir)).isDirectory());
 
-  const eventDirectories = ["guild", "client"];
+  let loadedEvents = [];
 
   for (const dir of eventDirectories) {
-   const events = fs.readdirSync(path.join(process.cwd(), "events", dir)).filter((file) => file.endsWith(".js"));
+   const events = fs.readdirSync(path.join(eventsPath, dir)).filter((file) => file.endsWith(".js"));
    for (const file of events) {
     const eventName = path.basename(file, ".js");
-    const filePath = path.join(process.cwd(), "events", dir, file);
+    const filePath = path.join(eventsPath, dir, file);
     const fileURL = pathToFileURL(filePath).href;
     const event = await import(fileURL);
-    defaultConfig.debugger.displayEventList && logger("event", `Loaded event ${eventName} from /events/${dir}/${file}`);
     client.on(eventName, event[eventName].bind(null, client));
+    loadedEvents.push(`${eventName} from /events/${dir}/${file}`);
    }
-   logger("event", `Loaded ${events.length} events from /events/${dir} in ${Math.round(performance.now() - loadTime)}ms`);
+  }
+
+  if (loadedEvents.length > 0) {
+   logger("event", `Loaded ${loadedEvents.length} events: ${loadedEvents.join(", ")} in ${Math.round(performance.now() - loadTime)}ms`);
   }
  } catch (error) {
   logger("error", `Error loading events: ${error.message}`);
